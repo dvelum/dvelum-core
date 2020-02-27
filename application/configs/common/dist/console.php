d3c673aaf945ad308ca8498b71b816a537ad0b36
Kirill <kirill.a.egorov@gmail.com>
2020-02-27 15:44:02 +0300

    Platform migration

--- application/configs/common/dist/console.php
+++ application/configs/common/dist/console.php
@@ -2,7 +2,6 @@
 return [
     'router' => '\\Dvelum\\App\\Router\\Console',
     'controller' => '\\Dvelum\\App\\Console\\Controller',
-    'user_id'=>1,
     'lockConfig'=>[
         'time_limit'=> 300,
         'intercept_timeout'=>300,
